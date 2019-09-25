fdce95a9e541ae4b24d92b78d77a7b8d1963e1f8
nolan295 <nolan796@live.ca>
2019-09-25 20:55:07 +0000

    100 barrel calculator in random.js

--- random.js
+++ random.js
@@ -1,9 +1,5 @@
-function makeLine(length) {
-    var line = "";
-    for (var j = 1; j <= length; j++) {
-      line += "* "
-    }
-    return line + "\n";
-  }
-
-  console.log(makeLine(10));
+function howManyHundreds(num){
+ howManyHundreds = (num / 100)
+ return Math.floor(howManyHundreds);
+}
+console.log(howManyHundreds(1001));
