be201cfab3a4616fa00e69b9b99bc6465baeca9b
nolan295 <nolan796@live.ca>
2019-09-25 19:00:05 +0000

    added hello.js file, changes to loopy.js, added a random tinkering document.

---
+++ loopy.js
@@ -0,0 +1,13 @@
+jeans = 0;
+
+while(jeans<201) {
+    if(jeans % 3 === 0 && jeans % 4 === 0){
+        console.log("LoopyLighthouse");
+    } else if(jeans % 3 === 0){
+        console.log("Loopy");
+    } else if(jeans % 4 === 0){
+        console.log("Lighthouse");
+    } jeans++
+}
+    
+
