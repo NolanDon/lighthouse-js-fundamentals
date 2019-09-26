6e6da2556614e2d3f7fda896a700cdec86080f5e
nolan295 <nolan796@live.ca>
2019-09-26 17:39:27 +0000

    changes to conditionals.js and random.js

--- conditionals.js
+++ conditionals.js
@@ -1,88 +1,23 @@
-const raining = true;
+var numbers = [
+  [243, 12, 23, 12, 45, 45, 78, 66, 223, 3],
+  [34, 2, 1, 553, 23, 4, 66, 23, 4, 55],
+  [67, 56, 45, 553, 44, 55, 5, 428, 452, 3],
+  [12, 31, 55, 445, 79, 44, 674, 224, 4, 21],
+  [4, 2, 3, 52, 13, 51, 44, 1, 67, 5],
+  [5, 65, 4, 5, 5, 6, 5, 43, 23, 4424],
+  [74, 532, 6, 7, 35, 17, 89, 43, 43, 66],
+  [53, 6, 89, 10, 23, 52, 111, 44, 109, 80],
+  [67, 6, 53, 537, 2, 168, 16, 2, 1, 8],
+  [76, 7, 9, 6, 3, 73, 77, 100, 56, 100]
+];
 
-if (raining) {
-  console.log("Don't forget your umbrella!");
-}
-
-
-const raining = true;
-const cold = false;
-
-if (raining) {
-  console.log("Don't forget your umbrella!");
-}
-
-if (cold) {
-  console.log("Make sure you pick out a scarf!");
-}
-
-console.log("Now you're ready to go outside!");
-
-
-
-const cold = false;
-
-if (cold) {
-  console.log("Make sure you pick out a scarf!");
-} else {
-  console.log("Short sleeves are fine.");
-}
-
-
-
-
-const temperature = 12;
-
-if (temperature < 0) {
-  console.log("Make sure you pick out a scarf!");
-} else if (temperature < 15) {
-  console.log("Short sleeves won't cut it!");
-} else {
-  console.log("Short sleeves are fine.");
-}
-
-console.log("Now you're ready to go outside!");
-
-
-const isCitizen = true;
-const age = 26;
-
-if (isCitizen && age > 18) {
-  console.log("You are eligible to vote.");
-}
-if (temperature < -40 || temperature > 40) {
-    console.log("Maybe going outside isn't such a great idea…");
-  }
-  if (!raining) {
-    console.log("Leave your umbrella at home!");
-  }
-
-
-
-
-
-
-
-
-
-
-Elementary School if age is below 13
-
-Secondary School if age is between 13 and 18 (both inclusive)
-
-Lighthouse Labs in all other cases.
-
-
-
-whichSchool = (age) => {
-    if(age < 13){
-        whichSchool = "Elementary School"
-    } else if (age >= 13 && age <= 18) {
-        whichSchool = "Secondary School"
-    } else {
-        whichSchool = "Lighthouse Labs"
+for (let r = 0; r < numbers.length; r ++ ) {
+    for (let c = 0; c < numbers[r].length; c ++)
+      {
+      if (numbers[r][c] % 2 === 0){
+          numbers[r][c] = 'even';
+      } else {
+          numbers[r][c] = 'odd';
     }
-      return whichSchool;
-};
-    
-      whichSchool(5);
+  }
+}
--- random.js
+++ random.js
@@ -1,5 +1,35 @@
-function howManyHundreds(num){
- howManyHundreds = (num / 100)
- return Math.floor(howManyHundreds);
+function calculateRectangleArea(length, width){
+if(length < 0 || width < 0){
+  return undefined;
+} else {
+  return (length * width);
+
 }
-console.log(howManyHundreds(1001));
+}
+
+function calculateTriangleArea(base, height){
+ if(base < 0 || height < 0){
+  return undefined
+ } else {
+  return (base * height / 2);
+}
+}
+
+function calculateCircleArea(radius){
+ if(radius < 0){
+  return undefined;
+ } else {
+  return (Math.PI * Math.pow(radius,2));
+}
+}
+
+console.log(calculateRectangleArea(10, 5));    
+console.log(calculateRectangleArea(1.5, 2.5)); 
+console.log(calculateRectangleArea(10, -5));    
+
+console.log(calculateTriangleArea(10, 5)); 
+console.log(calculateTriangleArea(3, 2.5)); 
+console.log(calculateTriangleArea(10, -5)); 
+
+console.log(calculateCircleArea(10)); 
+console.log(calculateCircleArea(3.5)); 
